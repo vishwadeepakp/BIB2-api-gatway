67d0045aa76876f32d7e17afc90c9abc1a935fbd
saloni <saloni18heatabit@gmail.com>
2026-08-07 23:26:50 +0530

    vercel deploye

--- server.js
+++ server.js
@@ -4,14 +4,8 @@ const app = require("./app");
 
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
     res.send("Getway is alive")
-}); 
-
-app.listen(PORT, '0.0.0.0', () => {
-
-    console.log(`🚀 API Gateway running on port ${PORT}`);
-
 });
 
 process.on('unhandledRejection', (reason, promise) => {
@@ -21,3 +15,10 @@ process.on('unhandledRejection', (reason, promise) => {
 process.on('uncaughtException', (error) => {
     console.error('Uncaught Exception thrown:', error);
 });
+
+module.exports = app;
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(PORT, '0.0.0.0', () => {
+        console.log(`🚀 API Gateway running on port ${PORT}`);
+    });
+}
